b5229ee6a5c98f5afffbbf6ebcadbd4b0beabe55
catkins <catkinsjr28@gmail.com>
2017-06-08 21:16:51 -0400

    Clickable Letters and Start Button

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,33 @@
-function gameStart(){
-	getElementById("start").style.display= "none";
-}
+$(".btn-primary").on("click", function(){
+	chooseWord();
+	
+});
+
+
 
 var wordsArray = ['outkast', 'migos', 'wale', 'nwa', 'eminem', 'tupac', 'biggie', 'common', 'pitbull', 'tyga'];
 var wordsUArray = [];
-var lives = 6;
+var lives = 10;
 var numInWordsArray = wordsArray.length; 
 var words = "";
 var wordsU = "";
 
+$(".btn-default").on("click", function(){
+	var guess = this.innerHTML.toLowerCase();
+	console.log(guess);
+});
+
 function chooseWord(){
-	words = wordsArray(Math.floor(Math.random()*numInWordsArray));
+	words = wordsArray[(Math.floor(Math.random()*numInWordsArray))];
+	console.log(words);
 }
 
 function setCharacters(){
-	chooseWord();
+	//chooseWord();
 	for (var i=0; i<words.length; i++){
 		wordsArray[i] = words.charAt(i);
 		wordsUArray[i] = "_";
-		
+		console.log(wordsUArray[i]);
 	}
 	wordsU = wordsUArray.join("");
 	document.getElementById("WORD").innerHTML = wordsU;
@@ -35,25 +44,25 @@ function updateLetter(letter){
 		}
 	}
 
-if (changes < 1){
-	lives -=1;
-	document.getElementById("lives").innerHTML - lives;
-	wordU = wordsArray.join("");
-	document.getElementById("WORD").innerHTML = wordsU;
-}
-	var words1 = wordsArray.join("");
-	var words2 = wordsUArray.join("");
+		if (changes < 1){
+			lives -=1;
+			document.getElementById("lives").innerHTML - lives;
+			wordU = wordsArray.join("");
+			document.getElementById("WORD").innerHTML = wordsU;
+		}
+			var words1 = wordsArray.join("");
+			var words2 = wordsUArray.join("");
 
-	if(words1 ==words2){  
-		alert("You Won! Loading a new word!");
-		window.location.reload();
-	}
-	if(lives < 1){
-		document.getElementById("WORD").innerHTML == words1;
-		alert("You have run out of lives. Please try again.");
-		window.location.reload();
-	}
+			if(words1 ==words2){  
+				alert("You Won! Loading a new word!");
+				window.location.reload();
+			}
+			if(lives < 1){
+				document.getElementById("WORD").innerHTML == words1;
+				alert("You have run out of lives. Please try again.");
+				window.location.reload();
+			}
 }
 
-chooseWord();
-setCharacters();
+//chooseWord();
+//setCharacters();
